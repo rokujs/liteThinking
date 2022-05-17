e35f2c96f06aeb3f1868924fc9413877b36ac123
rokujs <shiuxcode@gmail.com>
2022-05-17 14:40:25 -0500

    Change settings.py

--- backEnd/src/src/settings.py
+++ backEnd/src/src/settings.py
@@ -12,8 +12,16 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import environ
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
